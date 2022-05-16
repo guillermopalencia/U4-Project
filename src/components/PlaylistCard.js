c0ed9f3446d84f721462a9ea6b4f0270663a9803
Guillermo Palencia <guillermopalencia@yahoo.com>
2022-05-16 02:38:59 -0400

    created playlistdetail component

--- src/components/PlaylistCard.js
+++ src/components/PlaylistCard.js
@@ -1,9 +1,19 @@
 import { getPlaylists } from '../resources'
+import { useNavigate } from 'react-router'
 
 const PlaylistCard = ({ playlists }) => {
+  const navigate = useNavigate()
+
+  const nav = (id) => {
+    navigate(`/playlist/${id}`)
+  }
   let playlistarray = playlists
     ? playlists.map((playlist) => (
-        <div key={playlist.id} className="playlistCard">
+        <div
+          key={playlist.id}
+          className="playlistCard"
+          onClick={() => nav(playlist.id)}
+        >
           <div className="playlistsLeft">
             {playlist.images.length ? (
               <img src={playlist.images[0].url} />
