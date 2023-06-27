86aada9b1785b30c25704435913c87518023870f
gpalencia <gpalencia@google.com>
2023-06-27 10:26:45 -0400

    added basic test for deployment

---
+++ src/index.test.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './style/index.css'
+import App from './App'
+import reportWebVitals from './reportWebVitals'
+import { BrowserRouter as Router } from 'react-router-dom'
+
+import './App'
+
+describe('has content', () => {
+    it('has root', () => {
+        expect(App)
+    })
+})
