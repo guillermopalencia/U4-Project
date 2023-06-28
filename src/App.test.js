667f5dceb7b3e61681d8f4e76b0122d3a2f449ab
gpalencia <gpalencia@google.com>
2023-06-28 19:47:01 -0400

    created unit tests

--- src/App.test.js
+++ src/App.test.js
@@ -6,3 +6,29 @@ describe('has content', () => {
       expect(App)
   })
 })
+
+const testDiv = () => {
+  return render(
+    <div data-testid='cool'>
+      <h1>word</h1>
+      <h2>hot</h2>
+      <h3>fast</h3>
+    </div>
+  )
+}
+
+test('h1 content renders', () => {
+  const { getByText } = testDiv()
+
+  expect(getByText(/word/)).toBeInTheDocument()
+  expect(getByText(/fast/)).toBeInTheDocument()
+  expect(getByText(/hot/)).toBeInTheDocument()
+})
+
+test('div renders', () =>{
+
+  const { getByTestId } = testDiv()
+
+  expect(getByTestId(/cool/)).toBeInTheDocument()
+  expect(getByTestId(/cool/)).toHaveTextContent(/hot/)
+})
